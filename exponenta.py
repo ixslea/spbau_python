83cf554d50d7802af82c54b7fd7f5d95aa34186c
ixslea <83837721+ixslea@users.noreply.github.com>
2022-10-16 17:20:29 +0300

    задание 3

--- exponenta.py
+++ exponenta.py
@@ -2,6 +2,23 @@
 Вычисление экспоненты двумя функциями и вывод полученных значений
 """
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""Рисование графика"""
+
+
+def graph(start, end):
+    arguments = np.arange(start, end, 0.5)
+    plt.plot(  # функция для отображения графика
+       arguments,
+       [my_exp(a) for a in arguments], label='my_exp()')
+
+    plt.plot(  # функция для отображения графика
+       arguments,
+       [math.exp(a) for a in arguments], '--r', label='math.exp()')
+    plt.legend()
+    plt.show()
 
 
 def my_exp(x_arg):
@@ -18,5 +35,6 @@ def my_exp(x_arg):
 help(math.exp)
 help(my_exp)
 
-print(math.exp((5)))
-print(my_exp(5))
+print("Результат, встроенной функции:", math.exp(5))
+print("Результат, написанной функции:", my_exp(5))
+graph(0, 100)
